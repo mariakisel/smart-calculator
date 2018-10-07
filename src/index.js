7721fa54b76ba9f4aa159bed3b3c6eccd5faebcf
mariakisel <maria.kisialiova@gmail.com>
2018-10-07 21:03:20 +0300

    smart-calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+  this.str=String(initialValue); 
   }
 
   add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
-
-  multiply(number) {
-    // your implementation
-  }
-
-  devide(number) {
-    // your implementation
-  }
-
-  pow(number) {
-    // your implementation
-  }
+    this.str+="+"+String(number);
+    return new SmartCalculator(this.str);
+    }
+    
+    subtract(number) {
+    this.str+=("-"+String(number));
+    return new SmartCalculator(this.str);
+    }
+    
+    multiply(number) {
+    this.str+=("*"+String(number));
+    return new SmartCalculator(this.str);
+    }
+    
+    devide(number) {
+    this.str+=("/"+String(number));
+    return new SmartCalculator(this.str);
+    }
+    
+    
+    pow(number) {
+    let last= String(this.str.match(/\d+$/ig));
+    this.str=this.str.substring(0,this.str.length-last.length)+String(Math.pow(Number(last),number));
+    return new SmartCalculator(this.str);
+    }
+    
+    toString(){
+    return eval(this.str);
+    }
+    
 }
+    
+    module.exports = SmartCalculator;
+    
 
-module.exports = SmartCalculator;
+
